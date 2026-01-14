a8a87c378a139c346850343d558dbb4ba30044bc
Aree17 <arelys.ajila@unl.edu.ec>
2026-01-14 12:55:53 -0500

    Adición de modelo para Resultados

--- RYU/Resultados/models.py
+++ RYU/Resultados/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from EstructuraAcademica.models import PeriodoAcademico
+from Usuarios.models import Persona
+from Prueba.models import BancoPregunta, PreguntaOpcion
 
-# Create your models here.
+class Prueba(models.Model):
+    fecha_realizacion=models.DateTimeField(auto_now_add=True)
+    persona=models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='pruebas')
+    periodo_academico=models.OneToOneField(PeriodoAcademico, on_delete=models.CASCADE, related_name='pruebas')
+
+    def mostrarCarrerasAfines(self):
+        pass
+
+class ResultadoPorCarrera(models.Model):
+    banco_pregunta=models.ForeignKey(BancoPregunta, on_delete=models.CASCADE, related_name='resultados_por_carrera', default=None)
+    prueba=models.ForeignKey(Prueba, on_delete=models.CASCADE, related_name='resultados_por_carrera')
+
+    def _str_(self):
+        return f"Resultado de {self.carrera}"
+
+    def calcularResultado(resultado):
+        pass
+
+class Resultado(models.Model):
+    pregunta_opcion= models.ForeignKey(PreguntaOpcion, on_delete=models.CASCADE, related_name='resultados', default=None)
+    resultado_por_carrera = models.ForeignKey(ResultadoPorCarrera, on_delete=models.CASCADE, related_name='resultados')
+
+    def __str__(self):
+       return f"Resultado {self.id}"
+
+    def guardarTemporalmente(self):
+        pass
+
