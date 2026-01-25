2354d00e99dee468b734084e97770109ce64024f
Michu117 <fabricio.ruiz@unl.edu.ec>
2026-01-25 15:44:37 -0500

    Perfil
    perfil y editar contraseña

--- RYU/Usuarios/views.py
+++ RYU/Usuarios/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.db import transaction
@@ -118,3 +119,28 @@ def home(request):
     })
 
 
+@login_required
+def perfil_view(request):
+    cuenta = request.user.cuenta  # tu relación personalizada
+
+    return render(request, "auth/perfil.html", {
+        "user": request.user,
+        "cuenta": cuenta
+    })
+
+@login_required
+def cambiar_password_view(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+
+            messages.success(request, "Contraseña actualizada correctamente.")
+            return redirect("perfil")
+    else:
+        form = PasswordChangeForm(request.user)
+
+    return render(request, "auth/cambiar_password.html", {
+        "form": form
+    })
